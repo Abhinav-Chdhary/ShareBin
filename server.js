5be0e9498f0ef7d77c5af552bcb6c8ea5a7b6c67
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-20 15:49:48 +0530

    Connected to database

--- server.js
+++ server.js
@@ -6,27 +6,48 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 //to get data from request
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
+
+const Document = require("./models/Document");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/sharebin");
 
 //home page
 app.get("/", (req, res) => {
     const code = `Welcome to ShareBin!
 
-Use the commands at the top right corner to 
+Use the commands at the top right corner to
 create a new file share with others.`;
     res.render("code-display", { code });
 });
 
 //new page
-app.get("/new", (req, res)=>{
+app.get("/new", (req, res) => {
     res.render("new.ejs");
 });
 
 //save a text
-app.post("/save", (req, res)=>{
-    //logs the value
-    console.log(req.body.value);
+app.post("/save", async (req, res) => {
+    const value = req.body.value;
+    try{
+        const document = await Document.create({value})
+        res.redirect(`/${document.id}`)
+    }catch(e){
+        res.render("new", { value });
+    }
+    console.log(value);
 });
 
+//
+app.get("/:id", async (req, res)=>{
+    const id = req.params.id;
+    try{
+        const document = await Document.findById(id);
+        res.render("code-display", {code: document.value});
+    }
+    catch(e){
+        res.redirect("/");
+    }
+})
 
 app.listen(3000);
