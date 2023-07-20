84a66674c77f33a44cca6b1734d19df5a2b5b9e5
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-20 11:39:56 +0530

    Basic frontend

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+//view engines allow us to render web pages
+//using template files, one of them is embedded javaScript
+//view engines also have a template file that can take arguments
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) => {
+    const code = `Welcome to ShareBin!
+
+Use the commands at the top right corner to 
+create a new file share with others.`
+    res.render("code-display", {code});
+});
+
+app.listen(3000); 
