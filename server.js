ed1b9dcb1743bbecfe8d33790ceb13ce999b924f
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-20 11:59:18 +0530

    Styling and serving static content

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const app = express();
 //using template files, one of them is embedded javaScript
 //view engines also have a template file that can take arguments
 app.set("view engine", "ejs");
-
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     const code = `Welcome to ShareBin!
 
 Use the commands at the top right corner to 
-create a new file share with others.`
-    res.render("code-display", {code});
+create a new file share with others.`;
+    res.render("code-display", { code });
 });
 
-app.listen(3000); 
+app.listen(3000);
